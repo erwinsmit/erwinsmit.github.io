a743b026f8ab14aa01172c29e16df848fec1a431
esmit-inviqa <esmit@inviqa.com>
2015-05-27 22:01:31 +0100

    adding projcets.json and using handlebars to display project details

--- src/js/app/projects.js
+++ src/js/app/projects.js
@@ -1,21 +1,37 @@
 var Portfolio = Portfolio || {};
 Portfolio.Projects = (function ($) {
     "use strict";
+
+    var projects = '';
+
     return {
         init: function () {
-            this.projectSlider();
-            this.toggleProjectDetail();
+            this.fetchProjects();
         },
         projectSlider: function () {
-        	$('.highlight__gallery-slider').slick({
-        		prevArrow: '<button type="button" class="slick-prev"></button>',
-        		nextArrow: '<button type="button" class="slick-next"></button>'
-        	});
+            $('.highlight__gallery-slider').slick({
+                prevArrow: '<button type="button" class="slick-prev"></button>',
+                nextArrow: '<button type="button" class="slick-next"></button>'
+            });
         },
-        toggleProjectDetail: function () {
-        	$('.project-item').on('click', function() {
-        		$('.highlight').toggleClass('open'); 
-        	});
+        fetchProjects: function () {
+            $.getJSON("projects.json").done(function(data) {
+                projects = data;
+                Portfolio.Projects.showProjects();
+            });
+        },
+        showProjects: function () {
+            var source = $("#project-detail-template").html(),
+                template = Handlebars.compile(source);
+
+            $('.project-item').on('click', function () {
+                var projectId = $(this).data('project-id'),
+                    content = projects[projectId],
+                    html    = template(content);
+
+                $('.highlight').html(html).addClass('open');
+                Portfolio.Projects.projectSlider();
+            });
         }
     };
 })(window.jQuery);
