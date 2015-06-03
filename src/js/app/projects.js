eaaba85d2ccedc625c6871a0f6625bcb7a8205eb
esmit-inviqa <esmit@inviqa.com>
2015-06-03 22:04:05 +0100

    adding logo and about text

--- src/js/app/projects.js
+++ src/js/app/projects.js
@@ -1,37 +1,37 @@
 var Portfolio = Portfolio || {};
 Portfolio.Projects = (function ($) {
-    "use strict";
+  "use strict";
 
-    var projects = '';
+  var projects = '';
 
-    return {
-        init: function () {
-            this.fetchProjects();
-        },
-        projectSlider: function () {
-            $('.highlight__gallery-slider').slick({
-                prevArrow: '<button type="button" class="slick-prev"></button>',
-                nextArrow: '<button type="button" class="slick-next"></button>'
-            });
-        },
-        fetchProjects: function () {
-            $.getJSON("projects.json").done(function(data) {
-                projects = data;
-                Portfolio.Projects.showProjects();
-            });
-        },
-        showProjects: function () {
-            var source = $("#project-detail-template").html(),
-                template = Handlebars.compile(source);
+  return {
+    init: function () {
+      this.fetchProjects();
+    },
+    projectSlider: function () {
+      $('.highlight__gallery-slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        nextArrow: '<button type="button" class="slick-next"></button>'
+      });
+    },
+    fetchProjects: function () {
+      $.getJSON("projects.json").done(function (data) {
+        projects = data;
+        Portfolio.Projects.showProjects();
+      });
+    },
+    showProjects: function () {
+      var source = $("#project-detail-template").html(),
+        template = Handlebars.compile(source);
 
-            $('.project-item').on('click', function () {
-                var projectId = $(this).data('project-id'),
-                    content = projects[projectId],
-                    html    = template(content);
+      $('.project-item').on('click', function () {
+        var projectId = $(this).data('project-id'),
+          content = projects[projectId],
+          html = template(content);
 
-                $('.highlight').html(html).toggleClass('open');
-                Portfolio.Projects.projectSlider();
-            });
-        }
-    };
+        $('.highlight').html(html).toggleClass('open');
+        Portfolio.Projects.projectSlider();
+      });
+    }
+  };
 })(window.jQuery);
